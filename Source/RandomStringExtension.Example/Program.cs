3dae291f645bb921b9a8f2f496255ac9cc41e8cc
為什麼 <andy840119@gmail.com>
2018-09-28 12:12:05 +0800

    add initial project

---
+++ Source/RandomStringExtension.Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RandomStringExtension.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            //Introduce
+
+            //Sample
+
+
+            //Try yourself
+            while(true)
+            { 
+                var format = Console.ReadLine();
+                var randExample1 = rnd.NextString();
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
---
+++ Source/RandomStringExtension/RandomStringExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace System
+{
+    public static class RandomStringExtension
+    {
+        /// <summary>
+        /// D / a : Digital (0-9)
+        /// C / c : Upper case / Lower case char (A-z)
+        /// S / s : Symbol (!@#$%^&*)
+        /// A / a : Any char and digital ( Upper / Lower )
+        ///   *   : Any char and digital
+        ///   !   : And char, digital and symbol
+        /// </summary>
+        /// <param name="random"></param>
+        /// <param name="format"></param>
+        /// <param name="acceptSymbol"></param>
+        /// <returns></returns>
+        public static string NextString(this Random random,string format,string acceptSymbol = null)
+        { 
+            return "Hello";
+
+            char createRandomChar(char command)
+            { 
+                switch(command)
+                { 
+                    case 'A':
+                        return 
+                        
+                }
+            }
+        }
+    }
+}
