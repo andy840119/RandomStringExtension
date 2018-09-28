bfea4f4a32dffdc6a9e788352eda0df07ddcae23
為什麼 <andy840119@gmail.com>
2018-09-28 20:52:02 +0800

    實作邏輯部分

--- Source/RandomStringExtension.Example/Program.cs
+++ Source/RandomStringExtension.Example/Program.cs
@@ -9,16 +9,51 @@ namespace RandomStringExtension.Example
             Random rnd = new Random();
 
             //Introduce
+            // D / a : Digital (0-9)
+            // C / c : Upper case / Lower case char (A-z)
+            // S / s : Symbol (!@#$%^&*)
+            // A / a : Any char and digital ( Upper / Lower )
+            //   *   : Any char and digital
+            //   !   : And char, digital and symbol
 
-            //Sample
+            //Example : AD-57B2
+            DisplayExampleRandomString("CC-AAAA");
 
+            //Example : 82463251
+            DisplayExampleRandomString("DDDDDDDD");
+
+            //Example : dkgyleba
+            DisplayExampleRandomString("aaaaaaaa");
+
+            //Example : 2$#kED3B
+            DisplayExampleRandomString("!!!!!!!!");
+
+            //Example : +=--+++=
+            DisplayExampleRandomStringWithAcceptSymbol("SSSSSSSS", "+=-");
 
             //Try yourself
-            while(true)
+            while (true)
             { 
                 var format = Console.ReadLine();
-                var randExample1 = rnd.NextString();
-                Console.WriteLine("Hello World!");
+
+                if (string.IsNullOrEmpty(format))
+                    break;
+
+                DisplayExampleRandomString(format);
+            }
+
+            void DisplayExampleRandomString(string str)
+            {
+                Console.WriteLine(@"rnd.NextString(""" + str + @""")");
+                Console.WriteLine(rnd.NextString(str));
+                Console.WriteLine("======================");
+            }
+
+            void DisplayExampleRandomStringWithAcceptSymbol(string str,string acceptSymbol)
+            {
+                Console.WriteLine(@"rnd.NextString(""" + str + @""",""" + acceptSymbol + @""")");
+                Console.WriteLine(rnd.NextString(str, acceptSymbol));
+                Console.WriteLine("======================");
             }
         }
     }
--- Source/RandomStringExtension/RandomStringExtension.cs
+++ Source/RandomStringExtension/RandomStringExtension.cs
@@ -5,7 +5,7 @@ namespace System
     public static class RandomStringExtension
     {
         /// <summary>
-        /// D / a : Digital (0-9)
+        /// D / d : Digital (0-9)
         /// C / c : Upper case / Lower case char (A-z)
         /// S / s : Symbol (!@#$%^&*)
         /// A / a : Any char and digital ( Upper / Lower )
@@ -17,16 +17,46 @@ namespace System
         /// <param name="acceptSymbol"></param>
         /// <returns></returns>
         public static string NextString(this Random random,string format,string acceptSymbol = null)
-        { 
-            return "Hello";
+        {
+            var returnString = "";
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var digitals = "0123456789";
+            var symbols = !string.IsNullOrEmpty(acceptSymbol) ? acceptSymbol : "!@#$%^&*_-+=~";
+
+            foreach (var chr in format)
+            {
+                returnString += createRandomChar(chr);
+            }
+            return returnString;
 
             char createRandomChar(char command)
             { 
                 switch(command)
                 { 
-                    case 'A':
-                        return 
-                        
+                    case 'D' : case 'd' :
+                        return GetSingleCharInString(digitals);
+                    case 'C' :
+                        return GetSingleCharInString(chars);
+                    case 'c' :
+                        return GetSingleCharInString(chars.ToLower());
+                    case 'S' : case 's' :
+                        return GetSingleCharInString(symbols);
+                    case 'A' :
+                        return GetSingleCharInString(chars + digitals);
+                    case 'a' :
+                        return GetSingleCharInString(chars.ToLower() + digitals);
+                    case '*' :
+                        return GetSingleCharInString(chars + chars.ToLower() + digitals + digitals);//Digits shouuld be twice times because char is twice time
+                    case '!':
+                        return GetSingleCharInString(chars + chars.ToLower() + digitals + digitals + acceptSymbol + acceptSymbol);
+                    default :
+                        return command;
+
+                }
+
+                char GetSingleCharInString(string str)
+                {
+                    return str[random.Next(str.Length)];
                 }
             }
         }
